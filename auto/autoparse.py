56d5b09595011922d487e31e94fa9e3b91292134
drhodes <physci@gmail.com>
2010-01-08 15:33:09 -0500

    starting-parser-generation

--- auto/autoparse.py
+++ auto/autoparse.py
@@ -1,5 +1,8 @@
 from pyparsing import *
 
+# This is the grammar for the version of ebnf the Go team uses
+# this will eventually generate a parser :3  
+#------------------------------------------------------------------
 # Production  = production_name "=" Expression "." .
 # Expression  = Alternative { "|" Alternative } .
 # Alternative = Term { Term } .
@@ -16,15 +19,21 @@ Group       = Forward()
 Option      = Forward()
 Repetition  = Forward()
 
-EQUAL = Literal("=")
+EQUAL = Suppress("=")
 DOT = Literal(".")
 PIPE = Literal("|")
-DQUOTE = Literal('"')
+DQUOTE = Suppress('"')
 TICK = Literal("`")
 LPAREN, RPAREN, LBRACE, RBRACE, LBRACKET, RBRACKET, LANGLE, RANGLE = map(Literal, "(){}[]<>")
 
 
-token = (DQUOTE + Word(alphanums+"=()[]{}&!@#$%^&*|+=-/<>-;:_,.*'`") + DQUOTE |
+#------------------------------------------------------------------
+class TokenP(object):
+    def __init__(self, rule):
+        pass
+
+
+token = (DQUOTE + Word(alphanums+"!#$%&'()*+,-./:;<=>@[]^_`{|}") + DQUOTE |
          TICK + Or(map(Literal, alphanums+"""_."'""" + "\\")) + TICK )
          
 def tokenAction(result):
@@ -35,11 +44,24 @@ token.setParseAction(tokenAction)
 production_name = Word(alphas + "_")
 #------------------------------------------------------------------
 # Production  = production_name "=" Expression "." .
-Production << production_name + EQUAL + Expression + DOT
+class ProductionP(object):
+    def __init__(self, rule):
+        self.name = rule.asList()[0]
+        self.rule = rule
+    def __repr__(self):
+        return str(self.rule.asList())
+
+
+Production << production_name + EQUAL + Expression + Suppress(DOT)
+def productionAction(p):
+    return ProductionP(p)
+Production.setParseAction(productionAction)
+
 
 #------------------------------------------------------------------
 # Expression  = Alternative { "|" Alternative } .
-Expression << Alternative + ZeroOrMore(PIPE + Alternative) 
+# Expression << Alternative + ZeroOrMore(PIPE + Alternative) 
+Expression << Alternative + ZeroOrMore(Suppress(PIPE) + Alternative) 
 
 #------------------------------------------------------------------
 # Alternative = Term { Term } .
@@ -60,12 +82,29 @@ Group << (LPAREN + Expression + RPAREN)
 
 #------------------------------------------------------------------
 # Option      = "[" Expression "]" .
+class OptionP(object):
+    def __init__(self, rule):
+        self.rule = rule
+
+    def __repr__(self):
+        return "Optional( %s )" % (str(self.rule))
+
 Option << (LBRACKET + Expression + RBRACKET)
+Option.setParseAction(OptionP)
 
 
 #------------------------------------------------------------------
 # Repetition  = "{" Expression "}" .
+class RepetitionP(object):
+    def __init__(self, rule):
+        self.rule = rule
+
+    def __repr__(self):
+        return "ZeroOrMore( %s )" % (str(self.rule))
+
 Repetition << (LBRACE + Expression + RBRACE)
+Repetition.setParseAction(RepetitionP)
+
 
 
 Grammar = ZeroOrMore(Production)
